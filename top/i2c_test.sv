14e5bd77a9d961d81cb30f6822aa43cad2cf203f
Arpit-Singh22 <arpitsingh22@hotmail.com>
2026-02-09 08:41:52 +0000

    clock config test & created macro test reusability

--- top/i2c_test.sv
+++ top/i2c_test.sv
@@ -1,10 +1,12 @@
 class test_lib extends uvm_test;
 	`uvm_component_utils(test_lib)
 	`NEW_COMP
+	virtual wb_if vif;
 
 	i2c_env env;
 	function void build_phase(uvm_phase phase);
 	  env = i2c_env::type_id::create("env", this);
+	  uvm_config_db#(virtual wb_if)::get(this, "*","vif", vif);
 	endfunction
 
 	function void end_of_elaboration_phase(uvm_phase phase);
@@ -12,16 +14,21 @@ class test_lib extends uvm_test;
 	endfunction
 endclass
 
-class base_test extends test_lib;
-	`uvm_component_utils(base_test)
-	`NEW_COMP
-
-	task run_phase(uvm_phase phase);
-		base_seq_lib seq = base_seq_lib::type_id::create("seq");
-		phase.raise_objection(this);
-		super.run_phase(phase);
-		seq.start(env.agt.sqr);
-		phase.phase_done.set_drain_time(this, 100);
-		phase.drop_objection(this);
-	endtask
+`define I2C_TEST(TESTNAME, SEQ) \
+class TESTNAME extends test_lib; \
+	`uvm_component_utils(TESTNAME)\
+	`NEW_COMP \
+\
+	task run_phase(uvm_phase phase);\
+		SEQ seq = SEQ::type_id::create($sformatf(SEQ));\
+		phase.raise_objection(this);\
+		super.run_phase(phase);\
+		seq.start(env.agt.sqr);\
+		phase.phase_done.set_drain_time(this,2000);\
+		phase.drop_objection(this);\
+	endtask\
 endclass
+
+
+`I2C_TEST(config_clock_test, config_clock_seq)
+`I2C_TEST(write_test, write_seq)
--- top/top.sv
+++ top/top.sv
@@ -1,16 +1,21 @@
 module top;
 	reg clk;
 	logic rst_n;
+	
+	tri scl;
+	tri sda;
 
-	logic scl_pad_i;
-    logic scl_pad_o;
-    logic scl_padoen_o;
-    logic sda_pad_i;
-    logic sda_pad_o;
-    logic sda_padoen_o;
 	
 	wb_if pif(clk, rst_n);
 	i2c_if i2c_bus();
+	bind i2c_master_top i2c_assertions i2c_chk(
+		.wb_clk_i	(wb_clk_i),
+		.wb_rst_i	(wb_rst_i),
+		.tip		(tip),
+		.scl_pad_i	(scl_pad_i),
+		.sda_pad_i	(sda_pad_i)
+	);
+	i2c_slave_model slave_model(.scl(scl), .sda(sda));
 
 	i2c_master_top dut(
 		.wb_clk_i		(pif.wb_clk_i), 
@@ -24,30 +29,33 @@ module top;
 		.wb_cyc_i		(pif.wb_cyc_i), 
 		.wb_ack_o		(pif.wb_ack_o), 
 		.wb_inta_o		(pif.wb_inta_o),	
-		.scl_pad_i		(scl_pad_i), 
-		.scl_pad_o		(scl_pad_o), 
-		.scl_padoen_o	(scl_padoen_o),
-		.sda_pad_i		(sda_pad_i), 
-		.sda_pad_o		(sda_pad_o), 
-		.sda_padoen_o	(sda_padoen_o) 
+		.scl_pad_i		(i2c_bus.scl_pad_i), 
+		.scl_pad_o		(i2c_bus.scl_pad_o), 
+		.scl_padoen_o	(i2c_bus.scl_padoen_o),
+		.sda_pad_i		(i2c_bus.sda_pad_i), 
+		.sda_pad_o		(i2c_bus.sda_pad_o), 
+		.sda_padoen_o	(i2c_bus.sda_padoen_o) 
 );
-	always #5 clk = ~clk;
+	always #20 clk = ~clk;	//25Mhz
 	initial begin
 		clk = 0;
-		rst_n = 0;
-		repeat(3) @(posedge clk);
 		rst_n = 1;
+		repeat(3) @(posedge clk);
+		rst_n = 0;
 	end
 	
-	assign i2c_bus.scl = scl_padoen_o ? 1'bz : scl_pad_o;
-	assign i2c_bus.sda = sda_padoen_o ? 1'bz : sda_pad_o;
+	assign scl = (i2c_bus.scl_padoen_o) ? 1'bz : i2c_bus.scl_pad_o;
+	pullup(scl);
+	assign sda = (i2c_bus.sda_padoen_o) ? 1'bz : i2c_bus.sda_pad_o;
+	pullup(sda);
 
-	assign scl_pad_i = i2c_bus.scl;
-	assign sda_pad_i = i2c_bus.sda;
+	assign i2c_bus.scl_pad_i = scl;
+	assign i2c_bus.sda_pad_i = sda;
 	
 	initial begin
 	  uvm_config_db#(virtual wb_if)::set(null, "*", "vif", pif);
 	  uvm_config_db#(virtual i2c_if)::set(null, "*", "i2c_bus", i2c_bus);
-	  run_test("base_test");
+	  //run_test("config_clock_test");
+	  run_test("write_test");
 	end
 endmodule
