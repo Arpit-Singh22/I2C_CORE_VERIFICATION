45194c273d2c692de1ab3d717f9545ca45002856
Arpit-Singh22 <arpitsingh22@hotmail.com>
2026-02-17 09:39:43 +0000

    modified gitignore file

--- common/wb_if.sv
+++ common/wb_if.sv
@@ -1,13 +1,13 @@
 interface wb_if(input bit wb_clk_i, input bit wb_rst_i);
-	logic        arst_i;       // asynchronous reset
-	logic  [2:0] wb_addr_i;     // lower address bits
-	logic  [7:0] wb_dat_i;     // databus input
-	logic  [7:0] wb_dat_o;     // databus output
-	logic        wb_we_i;      // write enable input
-	logic        wb_stb_i;     // stobe/core select signal
-	logic        wb_cyc_i;     // valid bus cycle input
-	logic        wb_ack_o;     // bus cycle acknowledge output
-	logic        wb_inta_o;    // interrupt request signal output
+	bit        arst_i;       // asynchronous reset
+	bit  [2:0] wb_addr_i;     // lower address bits
+	bit  [7:0] wb_dat_i;     // databus input
+	bit  [7:0] wb_dat_o;     // databus output
+	bit        wb_we_i;      // write enable input
+	bit        wb_stb_i;     // stobe/core select signal
+	bit        wb_cyc_i;     // valid bus cycle input
+	bit        wb_ack_o;     // bus cycle acknowledge output
+	bit        wb_inta_o;    // interrupt request signal output
 
 	clocking drv_cb @(posedge wb_clk_i);
 		default input #1 output #0;
