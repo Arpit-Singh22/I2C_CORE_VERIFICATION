d548d2fca666fa21a80f39869f6005cd16246f32
Arpit-Singh22 <arpitsingh22@hotmail.com>
2026-02-17 09:40:20 +0000

    removed the two clock delay during resetting of the signal

--- master/wb_driver.sv
+++ master/wb_driver.sv
@@ -25,23 +25,27 @@ class wb_driver extends uvm_driver#(wb_tx);
 	endtask
 
 		task drive_tx(wb_tx tx);
-			@(vif.drv_cb);
-			vif.drv_cb.wb_addr_i <= tx.addr[2:0];
-			vif.drv_cb.wb_we_i  <= tx.wr_rd;
-			vif.drv_cb.wb_stb_i <= 1'b1;
-			vif.drv_cb.wb_cyc_i <= 1'b1;
+            @(vif.drv_cb);
+            vif.drv_cb.wb_addr_i <= tx.addr[2:0];
+            vif.drv_cb.wb_we_i   <= tx.wr_rd;
+            vif.drv_cb.wb_stb_i  <= 1'b1;
+            vif.drv_cb.wb_cyc_i  <= 1'b1;
 
-			if (tx.wr_rd == 1) vif.drv_cb.wb_dat_i <= tx.data;
-			do begin
-				@(vif.drv_cb);
-			end while (vif.drv_cb.wb_ack_o != 1'b1);
-			if(tx.wr_rd == 0 ) tx.data = vif.drv_cb.wb_dat_o;
+            if (tx.wr_rd == 1) vif.drv_cb.wb_dat_i <= tx.data;
+            
+            // Wait for ACK
+            do begin
+                @(vif.drv_cb);
+            end while (vif.drv_cb.wb_ack_o !== 1'b1); 
 
-			@(vif.drv_cb);
-			@(vif.drv_cb);
-			vif.drv_cb.wb_stb_i <= 0; 
-			vif.drv_cb.wb_cyc_i <= 0; 
-			vif.drv_cb.wb_addr_i <= 0; 
-			vif.drv_cb.wb_dat_i <= 0;
-	endtask	
+            if(tx.wr_rd == 0 ) tx.data = vif.drv_cb.wb_dat_o;
+
+            @(vif.drv_cb); 
+            vif.drv_cb.wb_stb_i  <= 0; 
+            vif.drv_cb.wb_cyc_i  <= 0; 
+            vif.drv_cb.wb_addr_i <= 0; 
+            vif.drv_cb.wb_dat_i  <= 0;
+            vif.drv_cb.wb_we_i   <= 0;
+            
+    endtask
 endclass
